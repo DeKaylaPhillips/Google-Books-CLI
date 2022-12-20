327b185b177c67c8ff3e1fc9491e946877df533d
DeKayla Phillips <dekaylanicole@gmail.com>
2022-12-20 17:04:07 -0500

    Comments added.

--- books_CLI.py
+++ books_CLI.py
@@ -33,30 +33,27 @@ def search_books():
     api_endpoint = f"https://www.googleapis.com/books/v1/volumes?q={search_query}+intitle:{search_query}&key={api_key}" # api endpoint for basic google books search queries
     response = requests.get(api_endpoint) # HTTP Get Request using the api_endpoint as the url w/ the search query and api_key variable included
     
-    if response.status_code == 200:
-        data = response.json()['items']
+    if response.status_code == 200: # if request to api_endpoint successful
+        data = response.json()['items'] # retrieve the "items" in the Json object containing the book data
 
-        for book in data:
-            volume_info = book['volumeInfo']
-            title = volume_info['title']
-            publisher = volume_info['publisher']
-            author_lists = volume_info['authors']
-            number_of_authors = len(author_lists)
+        for book in data: # iterate through all book information retrieved from the data
+            volume_info = book['volumeInfo'] # volumeInfo obj will hold all necessary information to be displayed to the user on the CLI
+            title = volume_info['title'] # grabs each title contained in the volumnInfo object  
+            publisher = volume_info['publisher'] # grabs each publishing company's name from the volumeInfo object
+            author_lists = volume_info['authors'] # grabs each list of authors from the volumeInfo object
+            num_of_authors = len(author_lists) # used to find out which books have multiple or only one author 
             
-            # TODO: LOOP THROUGH THE LIST OF AUTHORS. WANT TO DISPLAY WHETHER OR NOT EACH BOOK HAS ONLY 1 AUTHOR OR MORE THAN 1 AUTHOR
-            if number_of_authors > 1: 
-                for i, author in enumerate(author_lists):
-                    i = i + 1
-                    author = (f"Author {i}: {author}")
-                    print(author)
-            elif number_of_authors == 1:
-                for author in author_lists:
-                    author = (f"Author: {author}")
-                    print(author)
-                
-                    # print(f"\nTitle: {title}\n{authors}\nPublisher: {publisher}\n")
+            authors = [] # initiates authors variable to a list that will contain a authors who contributed to multiple books and their enumerated value/author string
+            if num_of_authors > 1: # if there is more than one author in each author list
+                for i, author in enumerate(author_lists): # iterate over the author list containing more than one author
+                    i = i + 1 # do not want the author's count to begin counting from 0, so i + 1
+                    authors.append(f"Author {i}: {author}") # each author and their associated number/string will be inserted into the authors list 
+                    author = '\n'.join(authors) # assigns the value of author to string values printed on top of one another (\n) which will display both authors and there their associated number
             
-        # book_information = list(filter(data))
-        
+            elif num_of_authors == 1: # else if there is only one author in the list of authors
+                for author in author_lists: # iterate over each singular author 
+                    author = (f"Author: {author}") # author is assigned the value of each singular author and will no longer be displayed in list format
 
+            print(f"\nTitle: {title}\n{author}\nPublisher: {publisher}\n")
+            
 search_books()
