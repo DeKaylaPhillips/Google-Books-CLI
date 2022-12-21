1e0a2832b0bc2b2d1a4786a536c7bd8ca3dfe102
DeKayla Phillips <dekaylanicole@gmail.com>
2022-12-20 19:37:47 -0500

    Code in working state. Fixing small bugs to and working through potential edge cases. All program requirements appear to be met.

--- books_CLI.py
+++ books_CLI.py
@@ -1,25 +1,11 @@
 import os
-from dotenv import load_dotenv
+
 import requests
 import pprint as pp
-
+from dotenv import load_dotenv
 load_dotenv()
 
-# def test_API():
-    # query = "Harry Potter" # Tests query variable in endpoint for later usage w/ user input
-    # api_key = os.environ['API_KEY'] # API key secured in .env file and placed in .gitignore, accessible through the os module
-    # api_endpoint = f"https://www.googleapis.com/books/v1/volumes?q={query}&key={api_key}" # api endpoint for basic google books search queries
-    # response = requests.get(api_endpoint) # HTTP Get Request using the api_endpoint as the url w/ the search query and api_key variable included
-
-    # try: 
-    #     if response.status_code == 200: # Ensure status code is OK
-    #         volume_info = response.json()['items'][0]['volumeInfo'] # Testing Accessibility Here - syntax to destructure json items object, destructure from array, access volumeInfo for one singular book                                                         
-            # pp.pprint({"Request Success": "JSON object displayed below."}) # If try successful, will print request success message to the console.
-            # pp.pprint(volume_info) # Displays singular JSON formatted volumeInfo object.
-    # except Exception as e:
-        # print(f"Error!: {str(e)}") # Exception raised if request error found.
-
-# test_API()
+reading_list = [] 
 
 def search_books():
     search_query = input("Hello! Welcome to the Google Bookstore.\nEnter a book to search for:\n")
@@ -27,16 +13,18 @@ def search_books():
         print("Please try to enter your search again!\n")
         search_books()
     else:
-        print(f"\nTop 5 related books for: '{search_query}'")
+        print(f"\nTop 5 Books Related to: '{search_query}'")
     
     api_key = os.environ['API_KEY'] # API key secured in .env file and placed in .gitignore, accessible through the os module
-    api_endpoint = f"https://www.googleapis.com/books/v1/volumes?q={search_query}+intitle:{search_query}&key={api_key}" # api endpoint for basic google books search queries
+    maxResults = 5
+    api_endpoint = f"https://www.googleapis.com/books/v1/volumes?q={search_query}+intitle:{search_query}&maxResults={maxResults}&key={api_key}" # api endpoint for basic google books search query - searches to ensure query is in title, cli displays only 5 results, and api_key included in endpoint
     response = requests.get(api_endpoint) # HTTP Get Request using the api_endpoint as the url w/ the search query and api_key variable included
-    
+
     if response.status_code == 200: # if request to api_endpoint successful
         data = response.json()['items'] # retrieve the "items" in the Json object containing the book data
 
-        for book in data: # iterate through all book information retrieved from the data
+        for i, book in enumerate(data): # iterate through all book information retrieved from the data
+            i = i + 1
             volume_info = book['volumeInfo'] # volumeInfo obj will hold all necessary information to be displayed to the user on the CLI
             title = volume_info['title'] # grabs each title contained in the volumnInfo object  
             publisher = volume_info['publisher'] # grabs each publishing company's name from the volumeInfo object
@@ -54,6 +42,29 @@ def search_books():
                 for author in author_lists: # iterate over each singular author 
                     author = (f"Author: {author}") # author is assigned the value of each singular author and will no longer be displayed in list format
 
-            print(f"\nTitle: {title}\n{author}\nPublisher: {publisher}\n")
-            
+            query_results = f"\n{i}. {title}\n{author}\nPublisher: {publisher}\n"
+            print(query_results)
+   
+        selection = input("Enter a book number to add it to your reading list.\n")
+        if not selection or int(selection) < 1 or int(selection) > 5:
+            print("Invalid book number. Please try your search again!\n")
+            search_books()
+        else:
+            selected_book_title = data[int(selection) - 1]['volumeInfo']['title']
+            reading_list.append(selected_book_title)
+            print(f"Successfully added {selected_book_title} to your Reading List!\n")
+
+        menu_options(reading_list)
+
+def menu_options(reading_list):
+    menu_selection = input("Enter 1 to search for more books. Enter 2 to view your reading list!\n")
+    if not menu_selection:
+        print("No menu selection. Returning to search.\n")
+        search_books()
+    elif menu_selection and int(menu_selection) == 1:
+        print("Returning to search interface.\n")
+        search_books()
+    elif menu_selection and int(menu_selection) == 2:
+        print(f"Reading List:\n{reading_list}\n")
+                
 search_books()
